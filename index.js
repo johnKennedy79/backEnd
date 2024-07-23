5ca9eea21f4bb23cf89becb9cfc5bc90b115f89c
John Kennedy <john,m,r,kennedy.jk@gmail.com>
2024-07-23 12:59:57 +0100

    images added

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 dotenv.config();
 app.use(cors());
 
-const images = [];
+const images = [
+  "https://www.team-bhp.com/forum/attachments/motorbikes/966810d1344414263-steampunk-holmes-bike-putsch-racing-widowmakr655x483.jpg",
+  "https://i.pinimg.com/originals/c5/2c/46/c52c46457adb07fa2bfa9cb62b36dbd8.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTadp-cg8Fj4TEcObw6-7UZK6NwL9Vw6EtHlw&s",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTieybYthzC8kIhcthRwuXPw7bVM_dpsIk4qA&s",
+];
 
 app.get("/", function (req, res) {
   res.json("this is my root");
